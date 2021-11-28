bed9247c01d2cef12856d3cb1c90531c23a5b492
Felix "xq" Queißner <git@masterq32.de>
2021-11-28 01:09:17 +0100

    Adds batch building.

--- build.zig
+++ build.zig
@@ -11,4 +11,21 @@ pub fn build(b: *std.build.Builder) void {
 
     const dynamic_lib = Sdk.createSharedLibrary(b, target, mode);
     dynamic_lib.install();
+
+    const common_targets = [_]std.zig.CrossTarget{
+        std.zig.CrossTarget.parse(.{ .arch_os_abi = "x86_64-windows-gnu" }) catch unreachable,
+        std.zig.CrossTarget.parse(.{ .arch_os_abi = "x86_64-linux-gnu" }) catch unreachable,
+        std.zig.CrossTarget.parse(.{ .arch_os_abi = "x86_64-linux-musl" }) catch unreachable,
+        std.zig.CrossTarget.parse(.{ .arch_os_abi = "x86_64-macos" }) catch unreachable,
+        std.zig.CrossTarget.parse(.{ .arch_os_abi = "aarch64-linux" }) catch unreachable,
+        std.zig.CrossTarget.parse(.{ .arch_os_abi = "aarch64-macos" }) catch unreachable,
+    };
+    const build_all = b.step("all", "Builds the libraries for a set of common targets");
+    for (common_targets) |cross_target| {
+        const slib = Sdk.createStaticLibrary(b, cross_target, mode);
+        const dlib = Sdk.createSharedLibrary(b, cross_target, mode);
+
+        build_all.dependOn(&slib.step);
+        build_all.dependOn(&dlib.step);
+    }
 }
